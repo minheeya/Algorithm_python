ae2dff6b92f63e20f4f9bb20d67c199ccc728744
minhee kang <himinhee1113@gmail.com>
2021-08-15 01:04:54 +0900

    https://leetcode.com/problems/valid-parentheses/submissions/

---
+++ leetcode_valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValid(self, s):
+        table = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        
+        for c in s:
+            if c not in table:
+                stack.append(c)
+            #stack이 비어있거나 짝이 안맞을경우
+            elif not stack or stack.pop() != table[c]:
+                return False
+        return len(stack) == 0  #input = '[' 인경우 예외처리
